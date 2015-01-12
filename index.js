73a4e9f68d6bb068027be55e48549132091169c5
Ubuntu <ubuntu@ip-172-31-19-66.us-west-2.compute.internal>
2015-01-12 16:35:06 +0000

    html edits

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
- var buf=fs.readFileSync('singlehtml.html');
+ var buf=fs.readFileSync('/views/singlehtml.html');
  var str=buf.toString();
  response.send(str)
 //'I think I got this now')
