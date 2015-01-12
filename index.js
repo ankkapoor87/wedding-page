9698071abedee4aa20c74fa717e190373184cf0f
Ubuntu <ubuntu@ip-172-31-19-66.us-west-2.compute.internal>
2015-01-12 16:32:41 +0000

    trying again

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var app = express();
+var fs=require('fs')
+
+//var buf=fs.readFileSync('\htmlfile.html');
+//var str=buf.toString();
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+ var buf=fs.readFileSync('singlehtml.html');
+ var str=buf.toString();
+ response.send(str)
+//'I think I got this now')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
